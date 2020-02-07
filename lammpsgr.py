04dad0cf62261773afe89f1135f6baa29d2366b2
mooreprestonb <mooreprestonb@gmail.com>
2020-02-07 11:29:58 -0500

    added selection of atom types

--- lammpsgr.py
+++ lammpsgr.py
@@ -177,15 +177,18 @@ parser.add_argument('-nbins',type=int,dest='nbins',default=100,help="Number of b
 parser.add_argument('-time',type=int,dest='time',default=10,help="Report and write progress every so many seconds")
 parser.add_argument('-max',type=float,dest='rmax',default=10,help="Max distancs in g(r)")
 parser.add_argument('-min',type=float,dest='rmin',default=0,help="Min Distance in g(r)")
+parser.add_argument('-types', type=int, nargs="+", dest='itypes', help="atoms types to processes")
 
 # read in arguments, now translate to variables
 args = parser.parse_args()
+print (args)
 nbins = args.nbins
 configname = args.input # 'test.lammpstrj'
 outfile = args.output
 itime = args.time
 rmax = args.rmax
 rmin = args.rmin
+itypes = args.itypes
 
 print("Processing",configname,"to",outfile)
 natoms = getlammpsatoms(configname)
@@ -193,42 +196,73 @@ print("Number of atoms:",natoms)
 
 types = getlammpstypes(configname,natoms)
 ntypes =len(types.keys())
-print("Number of atom types:",ntypes,types)
+stypes = str(ntypes)
+for k in sorted(types): # iterate over key, value pairs 
+    stypes += " " + str(k) + ":" + str(types[k])
+print("Number of atom types: ",stypes)
 
 box = getlammpsbox(configname)
 print("Initial box dimensions = ",box)
 
 nconf = getlammpsnconf(configname,natoms) # count configurations
 
+# check types
+if itypes == None:
+    print("Using all types")
+    itypes = []
+    for i in range(1,ntypes+1):
+        itypes.append(i)
+else :
+    for it in itypes:
+        if it not in types:
+            print("ERROR! type not found",it,types)
+            exit(1)
+    
 # allocate arrays
 pos = numpy.zeros((natoms,3))
 atypes = numpy.zeros(natoms,dtype=int)
-ninter = int(ntypes*(ntypes-1)/2+ntypes)
-hist = numpy.zeros((nbins,ninter+1)) # types 1-1, 1-2 ... 1-n, 2-2.... 2-n, ... n-n
-histnorm = numpy.zeros((nbins,ninter+1)) 
+
+toff = 1
+ioffmat = numpy.zeros((ntypes+1,ntypes+1),dtype=int)
+pairs = []
+for j in range(1,ntypes+1):
+    if j in itypes: # are we processing this type?
+        for i in range(1,ntypes+1):
+            if i in itypes: # are we processing this type?
+                if (i<j): # diagonal matrix...
+                    ioffmat[i][j] = ioffmat[j][i]
+                    # print("j<i",i,j,toff)
+                else:
+                    ioffmat[0][j] = -1
+                    ioffmat[i][0] = -1
+                    ioffmat[i][j] = toff
+                    toff += 1
+                    # print("i>=j",i,j,toff)
+                    pairs.append(str(j)+"-"+str(i))
+print("Offset Matrix\n",ioffmat)
+print("Pairs\n",pairs)
+
 dx = (rmax-rmin)/(nbins-1)
+#toff = int(ntypes*(ntypes-1)/2+ntypes)+1
+histnorm = numpy.zeros((nbins,toff)) 
+hist = numpy.zeros((nbins,toff)) # types 1-1, 1-2 ... 1-n, 2-2.... 2-n, ... n-n
 hist[:,0] = numpy.arange(rmin,dx*nbins+rmin,dx) # create bin values
 
-ioff = numpy.zeros(ntypes,dtype=int)
-ioff[0] = 1
-for i in range(1,ntypes):
-    ioff[i] = ioff[i-1]+ntypes-i
-#print(ioff)
-    
 # create norm array for historgram
 fact = 1./(4.*math.pi*dx)
 for i in range(nbins):
     histnorm[i][0] = 1
-    xpt = (i+.5)*dx+rmin
+    xpt = (i+0.5)*dx+rmin
     for j in range(ntypes):
         for k in range(j,ntypes):
-            joff = ioff[j] + k
-            histnorm[i][joff] = fact/(types[j+1]*types[k+1]*xpt*xpt)
-            # print(i,j,k,joff,fact,types[j+1],types[k+1],xpt)
-            if(j==k) :
-                histnorm[i][joff] *= 2.  # diagonal terms need a factor of 2
+            joff = ioffmat[j+1][k+1]
+            if(joff != 0):
+                histnorm[i][joff] = fact/(types[j+1]*types[k+1]*xpt*xpt)
+                # print(i,j,k,joff,fact,types[j+1],types[k+1],xpt)
+                if(j==k) :
+                    histnorm[i][joff] *= 2.  # diagonal terms need a factor of 2
 #print(fact,histnorm[:2,:])
-
+histnorm[-1,1:] *= 2 # last value only is 1/2 volume so needs factor of 2 
 # create header for file
 hdr = ""
 for i in range(len(sys.argv)):
@@ -237,9 +271,9 @@ hdr += "\n dz = " + str(dx) + " Types:" + str(ntypes)
 for k in sorted(types): # iterate over key, value pairs 
     hdr += " " + str(k) + ":" + str(types[k])
 hdr += "\n time "
-for j in range(ntypes):
-    for k in range(j,ntypes):
-        hdr += str(j+1) + "-" + str(k+1) + " " 
+for p in pairs: # add pairs to header
+    hdr += p+" "
+#
 f = open(configname,'r')
 nconfig = 0  # read initial configuration
 tnow = time.time()
@@ -255,30 +289,21 @@ while(readconfig(f,natoms,box,pos,atypes,nconfig)):
     svol2 += box[0]*box[1]*box[2]*box[0]*box[1]*box[2]
 
     for i in range(natoms-1):
-        dist = pos[i+1:]-pos[i]
-        dist = dist-numpy.floor(dist/box+.5)*box # get periodic boundaries
-        #for j in range(len(dist)):
-        #    dist[k] -= box[k]*(math.floor(dist[k]/box[k]+.5))
-        rdist = numpy.linalg.norm(dist,axis=1)
-        ibin = ((rdist-rmin)/dx).astype(int)
-        for j in range(len(rdist)):
-            if(ibin[j]>=0 and ibin[j]<nbins-1):
-                jn = atypes[i]-1
-                jm = atypes[i+1+j]-1
-                if(jn>jm): # make sure we are in correct values
-                    temp = jn
-                    jn = jm
-                    jm = temp
-                jbin = ioff[jn] + jm
-                hist[ibin[j]][jbin] += 1
-                #print(i,i+1+j,pos[i],pos[i+1+j],rdist[j],ibin[j],jbin,ntypes,jn,jm)
-                #frac = (rdist[j]-rmin)/dx - ibin[j]
-                #hist[ibin[j]  ][jbin] += 1.-frac
-                #hist[ibin[j]+1][jbin] += frac                
-            #else:
-                #print("Warning: atom position out of range",i,j,pos[i],pos[j],rdist)
-                #ibin = max(ibin,0)
-                #ibin = min(ibin,nbins-1)
+        if(ioffmat[atypes[i]][0]==-1):
+            dist = pos[i+1:]-pos[i]
+            dist = dist-numpy.floor(dist/box+.5)*box # get periodic boundaries
+            #for j in range(len(dist)):
+            #    dist[k] -= box[k]*(math.floor(dist[k]/box[k]+.5))
+            rdist = numpy.linalg.norm(dist,axis=1)
+            ibin = ((rdist-rmin)/dx).astype(int)
+            for j in range(len(rdist)):                
+                if(ibin[j]>=0 and ibin[j]<nbins-1 and (ioffmat[0][atypes[i+1+j]] == -1)):
+                    jbin = ioffmat[atypes[i]][atypes[i+1+j]]
+                    #print(i,i+1+j,pos[i],pos[i+1+j],rdist[j],ibin[j],jbin,ntypes,jn,jm)
+                    #hist[ibin[j]][jbin] += 1
+                    frac = (rdist[j]-rmin)/dx - ibin[j]
+                    hist[ibin[j]  ][jbin] += 1.-frac
+                    hist[ibin[j]+1][jbin] += frac                
 
     tnowi = time.time()
     if(itime < tnowi-tnow):
@@ -294,6 +319,6 @@ while(readconfig(f,natoms,box,pos,atypes,nconfig)):
 avol = svol/nconfig
 stdvol = math.sqrt((svol2/nconfig - avol*avol))
 print("# configs read in:",nconfig," <vol> =",avol, "stdvol = ",stdvol)
-#print(hist)
 histnorm[:,1:] *= avol/nconfig
 savehist(outfile,hist,nconfig,hdr,histnorm)
+
