0d0ebad3eafb7c816bd273e7c9e1e0fce94502dc
Preston B. Moore <mooreprestonb@gmail.com>
2020-07-23 10:30:47 -0400

    added skewness kurtosis and ranges to data analysis

--- lammpslogavg.py
+++ lammpslogavg.py
@@ -1,35 +1,47 @@
 #!/usr/bin/python3
-
 # parse lammps log files
 
 import sys
 import numpy
+from scipy.stats import skew,kurtosis
 
 fi = open(sys.argv[1],"r")
 lines = fi.readlines()
 fi.close()
 
 ls = 0
+ns = 0
 for l in lines:
-    w1 = l.split()[0]
+    words = l.split()
     ls += 1
-    if(w1=="Step"):
+    if(len(words)>0 and words[0]=="Step"):
+        if(ns!=0):
+            print("Warning more then one \"Step\" found (using latest)")
+        ns = ls
         #print("found step")
-        break
+        #break
 
-header = lines[ls-1].split()
+if(ns==0):
+    print("Didn't find \"Step\" in lammps log file... exiting")
+    exit(1)
+    
+header = lines[ns-1].split()
 nc = len(header)
-n = len(lines[ls:])
-print(len(header),"Columns of data")
-print(n,"lines of data")
+n = len(lines[ns:])
 data = numpy.empty((nc,n),dtype=float)
+nd = 0
 for l in range(n):
-    words = lines[ls+l].split()
+    words = lines[ns+l].split()
     if(words[0].isdigit()==False):
         break
     for i in range(nc):
         data[i][l] = float(words[i])
-
+    nd += 1
+#print(data[0][:nd])
+print("#",sys.argv,len(header),"columns of data, with",nd,"lines of data")
+print("# column header average stdev skew kurtosis min max range")
 for i in range(nc):
-    print(i,header[i],numpy.average(data[i]),numpy.std(data[i]))
-
+    da = data[i][:nd]
+    dmin = numpy.min(da)
+    dmax = numpy.max(da)
+    print(i,header[i],numpy.average(da),numpy.std(da),skew(da),kurtosis(da),dmin,dmax,dmax-dmin)
