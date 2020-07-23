362f12ddf9beb37d00c334511d051165d55778c3
Preston B. Moore <mooreprestonb@gmail.com>
2020-07-23 10:37:10 -0400

    added command line argument to header

--- lammpslogavg.py
+++ lammpslogavg.py
@@ -28,7 +28,7 @@ if(ns==0):
 header = lines[ns-1].split()
 nc = len(header)
 n = len(lines[ns:])
-data = numpy.empty((nc,n),dtype=float)
+data = numpy.empty((nc,n),dtype=float) #maybe more then we need :-)
 nd = 0
 for l in range(n):
     words = lines[ns+l].split()
@@ -41,7 +41,7 @@ for l in range(n):
 print("#",sys.argv,len(header),"columns of data, with",nd,"lines of data")
 print("# column header average stdev skew kurtosis min max range")
 for i in range(nc):
-    da = data[i][:nd]
+    da = data[i][:nd] # only data that was parsed
     dmin = numpy.min(da)
     dmax = numpy.max(da)
     print(i,header[i],numpy.average(da),numpy.std(da),skew(da),kurtosis(da),dmin,dmax,dmax-dmin)
