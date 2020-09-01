40c9b6160dbeb357b5aff50d7e3644b16e4520f3
Preston B. Moore <mooreprestonb@gmail.com>
2020-09-01 15:30:07 -0400

    modified to have a max of integration and take argument 1

--- math/integrate.py
+++ math/integrate.py
@@ -1,9 +1,24 @@
 #!/usr/bin/python3
 
+import sys
 import numpy
 from scipy.integrate import simps
 
-filename = "0.5mgr6all.dat"
+def traptest(ydat,xdat):
+    IT = numpy.trapz(ydat,xdat) # trapazod rule
+    print(IT)
+
+    # trapazod rule by hand (slow)
+    sum = 0
+    for i in range(len(xdata1)-1):
+        sum += ((ydata1[i]+ydata1[i+1])/2)*(xdata1[i+1]-xdata1[i])
+        
+    print(IT,sum,abs(IT-sum))
+
+
+filename = sys.argv[1]
+#filename = "0.5mgr6all.dat"
+maxx = 15
 data = numpy.loadtxt(filename)
 
 #print(data)
@@ -11,12 +26,20 @@ data = numpy.loadtxt(filename)
 xdata = data[:,0] # get first column
 ydata = data[:,1] # get second column
 
-IS = simps(ydata,xdata)
-IT = numpy.trapz(ydata,xdata)
-print(IT,IS)
+# find maxx index and linear extrapolate
+idx = numpy.searchsorted(xdata,15)
+#print(idx,xdata[idx-1:idx+1],ydata[idx-1:idx+1])
 
-sum = 0
-for i in range(len(xdata)-1):
-    sum += ((ydata[i]+ydata[i+1])/2)*(xdata[i+1]-xdata[i])
+xdata1 = xdata[:idx+1]
+ydata1 = ydata[:idx+1]
+dx = xdata1[-1]-xdata1[-2]
+dy = ydata1[-1]-ydata1[-2]
+#print (xdata1[-2:],ydata1[-2:],dy,dx,(dy/dx),(maxx-xdata1[-2]))
+xdata1[-1] = maxx
+ydata1[-1] = ydata1[-2] + (maxx-xdata1[-2])*dy/dx
+#print (xdata1[-2:],ydata1[-2:])
 
-print(sum)
+IS = simps(ydata1,xdata1) # simpsons rule
+print(IS)
+#traptest(ydata1,xdata1)
+
